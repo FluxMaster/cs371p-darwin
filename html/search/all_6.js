30cffceabc6da4a59c98603e28aebebbfeff0df5
Andrew Polasek <andy@polasek.com>
2015-11-05 16:12:49 -0600

    I think this is finally final

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['real',['real',['../classSpecies.html#ac458aa1dfe5f7a72686323339f8412ec',1,'Species::real()'],['../classCreature.html#aee075e42b38c6e82f8cb6ef652be4427',1,'Creature::real()']]],
-  ['render',['render',['../classSpecies.html#a77bad9b75b82dbf719abe87925ef3745',1,'Species::render()'],['../classCreature.html#abede6ea489d2f9731b88e08923733ea2',1,'Creature::render()']]],
   ['request_5fhop',['request_hop',['../classDarwin.html#a64e26d5a4dea520350c2e43e1aaebb57',1,'Darwin']]],
   ['request_5finfect',['request_infect',['../classDarwin.html#a700cab7603364e9b303d7ff81a91bd27',1,'Darwin']]]
 ];
